22d544e1083cba4c363adfa9976c95652268246e
Brent <brentcarey1@outlook.com>
2020-03-11 17:15:46 +0000

    Added name to snake on screen

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 const net = require('net');
+const stdin = process.stdin;
 
+
+
+
+  
 /**
  * Establishes connection with the game server
  */
@@ -11,10 +16,27 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  stdin.setEncoding('utf8');
   
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+
+  stdin.on('data', (input) => {
+    let name = input[0] + input[1] + input[2]
+   
+  console.log('Name:', name);
+  conn.write(name);
+ 
+  
+});
+
+  conn.on('connect', () => {
+    console.log('Connected to the server');
+    
+  });
+
+
   return conn;
 }
 
